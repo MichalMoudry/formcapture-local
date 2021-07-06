f10d23b0b7c7a8959cdf8064120a54c24a66642a
MichalMoudry <moudry.michal@live.com>
2021-07-06 18:28:42 +0200

    Added page for adding templates + changes to home page (style of links to user doc)

--- FormCaptureLocal/wwwroot/js/main.js
+++ FormCaptureLocal/wwwroot/js/main.js
@@ -4,3 +4,32 @@
     const hash = await crypto.subtle.digest("SHA-256", encoder.encode(string));
     return Array.from(new Uint8Array(hash)).map(b => b.toString(16).padStart(2, "0")).join("");
 }
+
+function confirmDialog(message) {
+    var res = confirm(message);
+    return res;
+}
+
+function closeAlert(alertID) {
+    $("#" + alertID).hide();
+}
+
+function displayToast(toastID) {
+    document.getElementById(toastID).classList.remove("d-none");
+    $("#" + toastID).show();
+}
+
+function getImageProperties(imageID) {
+    var img = document.getElementById(imageID);
+    return img.naturalWidth + "|" + img.naturalHeight;
+}
+
+function executeJS(code) {
+    try {
+        eval(code);
+        return true;
+    } catch (e) {
+        console.error("Execution of code failed.");
+        return false;
+    }
+}
