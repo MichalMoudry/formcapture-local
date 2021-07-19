2b5980c1f399b32f141b4e8257e084ba20bfc1ab
MichalMoudry <moudry.michal@live.com>
2021-07-19 11:02:11 +0200

    Added tests for logout page + created basic structure for queue list page

---
+++ FormCaptureLocalTests/LogoutPageTests.cs
@@ -0,0 +1,24 @@
+﻿using Blazored.LocalStorage;
+using Bunit;
+using FormCaptureLocal.Pages.App;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FormCaptureLocalTests
+{
+    public class LogoutPageTests
+    {
+        [Fact]
+        public async Task LogoutTest()
+        {
+            using var context = new TestContext();
+            context.AddBlazoredLocalStorage();
+            var localStorage = context.Services.GetService<ILocalStorageService>();
+            await localStorage.SetItemAsync("LastLoginDate", DateTime.Now);
+            context.RenderComponent<Logout>();
+            bool containsLastLoginDate = await localStorage.ContainKeyAsync("LastLoginDate");
+            Assert.False(containsLastLoginDate);
+        }
+    }
+}
