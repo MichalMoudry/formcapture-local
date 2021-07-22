1d1b0e1d350ffa909ba63dda5cb1e32ec49f4f5b
MichalMoudry <moudry.michal@live.com>
2021-07-22 14:02:36 +0200

    Added localizer to solution + added localization to index page

--- FormCaptureLocal/Program.cs
+++ FormCaptureLocal/Program.cs
@@ -1,5 +1,7 @@
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
 using System.Threading.Tasks;
 
 namespace FormCaptureLocal
@@ -13,6 +15,8 @@ namespace FormCaptureLocal
 
             //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+            builder.Services.AddSingleton<IStringLocalizer<App>, StringLocalizer<App>>();
 
             await builder.Build().RunAsync();
         }
