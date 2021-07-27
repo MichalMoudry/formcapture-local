1947dfe472564e718af72b500de763703a9e1b66
MichalMoudry <moudry.michal@live.com>
2021-07-27 10:08:21 +0200

    Added error messages and tests for TestsAdd page

---
+++ FormCaptureLocalTests/TasksAddPageTests.cs
@@ -0,0 +1,54 @@
+﻿using Bunit;
+using FormCaptureLocal;
+using FormCaptureLocal.Pages.App.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using Xunit;
+
+namespace FormCaptureLocalTests
+{
+    public class TasksAddPageTests
+    {
+        /// <summary>
+        /// Test method for testing if error message is displayed properly.
+        /// </summary>
+        [Fact]
+        public void EmptyTaskContentTest()
+        {
+            using var context = new TestContext();
+            //Add services to context
+            context.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+            context.Services.AddSingleton<IStringLocalizer<App>, StringLocalizer<App>>();
+            context.JSInterop.SetupVoid("displayToast", "error-toast");
+            //Get localizer
+            var localizer = context.Services.GetService<IStringLocalizer<App>>();
+            //Render component
+            var tasksAddComponent = context.RenderComponent<TasksAdd>();
+            //Click test button
+            tasksAddComponent.Find("#testButton").Click();
+            var errorMessage = tasksAddComponent.Find(".errorMessage").TextContent;
+            Assert.Equal($"{localizer["TasksAddPageEmptyTaskContentErrorMessage"]}.", errorMessage);
+        }
+
+        /// <summary>
+        /// Test method for testing if error message is displayed properly.
+        /// </summary>
+        [Fact]
+        public void EmptyTaskNameTest()
+        {
+            using var context = new TestContext();
+            //Add services to context
+            context.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+            context.Services.AddSingleton<IStringLocalizer<App>, StringLocalizer<App>>();
+            context.JSInterop.SetupVoid("displayToast", "error-toast");
+            //Get localizer
+            var localizer = context.Services.GetService<IStringLocalizer<App>>();
+            //Render component
+            var tasksAddComponent = context.RenderComponent<TasksAdd>();
+            //Click submit button
+            tasksAddComponent.Find("#submitButton").Click();
+            var errorMessage = tasksAddComponent.Find(".errorMessage").TextContent;
+            Assert.Equal($"{localizer["TasksAddPageEmptyTaskNameErrorMessage"]}.", errorMessage);
+        }
+    }
+}
--- FormCaptureLocalTests/TemplatesListPageTests.cs
+++ FormCaptureLocalTests/TemplatesListPageTests.cs
@@ -15,12 +15,14 @@ namespace FormCaptureLocalTests
         public void EmptyTemplatesSortErrorTest()
         {
             using var context = new TestContext();
+            //Add services to context
             context.Services.AddLocalization(options => options.ResourcesPath = "Resources");
             context.Services.AddSingleton<IStringLocalizer<App>, StringLocalizer<App>>();
             context.JSInterop.Setup<List<Template>>("getAllItems", "Templates");
             context.JSInterop.SetupVoid("displayToast", "error-toast");
             var templatesListComponent = context.RenderComponent<TemplatesList>();
             var templatesCount = templatesListComponent.FindAll(".pointer").Count;
+            //Get localizer
             var localizer = context.Services.GetService<IStringLocalizer<App>>();
             Assert.Equal(0, templatesCount);
             templatesListComponent.Find(".dropdown-item").Click();
